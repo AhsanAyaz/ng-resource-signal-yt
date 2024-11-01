260680c4a78383d4b9d7f0878417bf2d1c5a80c5
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-11-02 00:40:11 +0100

    chore(app): final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {
   Component,
-  effect,
   inject,
   Injector,
   linkedSignal,
+  resource,
   signal,
   WritableSignal,
 } from '@angular/core';
@@ -11,51 +11,71 @@ import { RouterOutlet } from '@angular/router';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
 import { Comment, Post, PostService } from './services/post.service';
-import { firstValueFrom } from 'rxjs';
 import { User, USERS } from './constants/users';
+import { AlertComponent } from './components/alert/alert.component';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoListComponent, TodoDetailsComponent],
+  imports: [
+    RouterOutlet,
+    TodoListComponent,
+    TodoDetailsComponent,
+    AlertComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   postService = inject(PostService);
   injector = inject(Injector);
-  comments = signal<Comment[]>([]);
   users = signal(USERS);
   selectedUser = signal<User>(USERS[0]);
-  posts = signal<Post[]>([]);
-  loadingPosts = signal<boolean>(false);
-  loadingComments = signal<boolean>(false);
 
   selectedPost: WritableSignal<Post | null> = linkedSignal({
     source: this.selectedUser,
     computation: () => null,
   });
 
-  getUserPosts = effect(async () => {
-    const user = this.selectedUser();
-    this.loadingPosts.set(true);
-    const posts = await firstValueFrom(this.postService.getPosts(user.id));
-    this.posts.set(posts);
-    this.loadingPosts.set(false);
+  userPosts = rxResource<Post[], User>({
+    request: () => this.selectedUser(),
+    loader: ({ request: user }) => {
+      return this.postService.getPosts(user.id);
+    },
   });
 
-  getPostComments = effect(async () => {
-    const post = this.selectedPost();
-    if (!post) {
-      return;
-    }
-    this.loadingComments.set(true);
-    const comments = await firstValueFrom(
-      this.postService.getPostComments(post.id)
-    );
-    this.comments.set(comments);
-    this.loadingComments.set(false);
+  postComments = rxResource<Comment[], Post | null>({
+    request: () => this.selectedPost(),
+    loader: ({ request: post }) => {
+      if (!post) {
+        return of([]);
+      }
+      return this.postService.getPostComments(post.id);
+    },
   });
+
+  // userPosts = resource<Post[], User>({
+  //   request: () => this.selectedUser(),
+  //   loader: ({ request: user }) => {
+  //     return fetch(
+  //       `https://jsonplaceholder.typicode.com/users/${user.id}/posts`
+  //     ).then((res) => res.json());
+  //   },
+  // });
+
+  // postComments = resource<Comment[], Post | null>({
+  //   request: () => this.selectedPost(),
+  //   loader: ({ request: post }) => {
+  //     if (!post) {
+  //       return Promise.resolve([]);
+  //     }
+  //     return fetch(
+  //       `https://jsonplaceholder.typicodeaaaa.com/posts/${post.id}/comments`
+  //     ).then((res) => res.json());
+  //   },
+  // });
 
   postClick(post: Post) {
     this.selectedPost.set(post);
